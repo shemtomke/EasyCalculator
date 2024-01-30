7c8b4b8235faaf113e1c8f56c39a21e8479326ea
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2024-01-30 20:10:11 +0300

    Theme

--- app/src/main/java/com/example/easycalculator/MainActivity.java
+++ app/src/main/java/com/example/easycalculator/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.easycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTv, solutionTv;
+    MaterialButton buttonC, buttonBracketOpen, buttonBracketClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonAdd, buttonSubtract, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
