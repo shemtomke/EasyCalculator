f337d54310dd89aee374090a44ab5152a06bde98
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2024-01-30 22:23:20 +0300

    Calculation

--- app/src/main/java/com/example/easycalculator/MainActivity.java
+++ app/src/main/java/com/example/easycalculator/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     TextView resultTv, solutionTv;
     MaterialButton buttonC, buttonBracketOpen, buttonBracketClose;
     MaterialButton buttonDivide, buttonMultiply, buttonAdd, buttonSubtract, buttonEquals;
@@ -25,6 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         solutionTv = findViewById(R.id.solution_tv);
 
         assignId(buttonC, R.id.button_c);
+        assignId(buttonBracketOpen, R.id.button_open_bracket);
+        assignId(buttonBracketClose, R.id.button_close_bracket);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_point);
+        assignId(buttonAdd, R.id.button_plus);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonSubtract, R.id.button_subtract);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
     }
     void assignId(MaterialButton btn, int id){
         btn = findViewById(id);
@@ -35,6 +56,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         MaterialButton button = (MaterialButton) v;
         String buttonText = button.getText().toString();
-        solutionTv.setText(buttonText);
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if(buttonText.equals("AC"))
+        {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if(buttonText.equals("="))
+        {
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if(buttonText.equals("C"))
+        {
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length()-1);
+        }
+        else
+        {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult((dataToCalculate));
+
+        if(!finalResult.equals("Err")){
+            resultTv.setText(finalResult);
+        }
+    }
+    String getResult(String data)
+    {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if(finalResult.endsWith(".0"))
+            {
+                finalResult = finalResult.replace(".0", "");
+            }
+            return finalResult;
+        }catch (Exception e)
+        {
+            return "Err";
+        }
     }
 }
