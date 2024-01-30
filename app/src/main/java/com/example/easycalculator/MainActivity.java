dbaa9b45d70c06361a16ff16bef5bddd3bdd4003
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2024-01-30 20:16:33 +0300

    Onclick

--- app/src/main/java/com/example/easycalculator/MainActivity.java
+++ app/src/main/java/com/example/easycalculator/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.easycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView resultTv, solutionTv;
     MaterialButton buttonC, buttonBracketOpen, buttonBracketClose;
@@ -20,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
 
+        assignId(buttonC, R.id.button_c);
+    }
+    void assignId(MaterialButton btn, int id){
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        MaterialButton button = (MaterialButton) v;
+        String buttonText = button.getText().toString();
+        solutionTv.setText(buttonText);
     }
 }
